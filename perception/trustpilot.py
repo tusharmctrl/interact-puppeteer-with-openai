2f06f23ed0723b8c45a26be563453ec8c9b1e16c
tusharmctrl <tushar@missionctrl.dev>
2024-07-16 11:00:34 +0530

    Prompt Changes in Trustpilot Script

--- perception/trustpilot.py
+++ perception/trustpilot.py
@@ -1,18 +1,20 @@
 from crewai import Agent, Task, Crew
 from langchain_openai import ChatOpenAI
-from crewai_tools import WebsiteSearchTool , ScrapeWebsiteTool
-import os 
+from crewai_tools import ScrapeWebsiteTool
 from dotenv import load_dotenv
 from textwrap import dedent
-
 load_dotenv()
-api_key = os.getenv('API_KEY')
-# Model = 'gpt-3.5-turbo'
+
 Model = 'gpt-4o'
-llm = ChatOpenAI(model=Model,api_key=api_key,temperature=0)
+llm = ChatOpenAI(model=Model, temperature=0)
 
 web_rag_tool = ScrapeWebsiteTool()
 
+def get_user_input():
+    url = input("Please enter the URL of the website you want to analyze on Trustpilot.com: ")
+    return url
+
+url = get_user_input()
 
 review_retriever = Agent(
     role='Trustpilot.com Reviews Retriever',
@@ -28,16 +30,17 @@ reviewes_examiner = Agent(
     goal="Your task is to analyze the reviews we have received from review_retriever agent. For each journey, you need to review all the feedback and determine if the customers are satisfied. Your goal is to extract insights from the reviews and assess whether the journey meets customer expectations. You will identify key trends, positive highlights, and areas needing improvement. Your analysis will help us understand customer sentiments and make informed decisions to enhance our services. By thoroughly examining each review, you will ensure that we address any issues and continue to provide a high-quality experience for our customers.",
     backstory="""With a background in user experience in platform, you bring extensive experience in examining user reviews and get the output accordingly for any specialised category. Your expertise allows you to extract meaningful insights from diverse customer feedback, helping businesses understand consumer sentiment and make informed decisions. Your work contributes directly to enhancing the website's understanding of customer perceptions and improving its overall customer experience strategy. You have to review all the customer reviews and then generate a short desciption for each category / journey from those reviews.
     We have following categories - review all the user feedbacks and categorise those reviews under following categories, 
-    1. Entry and Home page related
-    2. Registration
-    3. Signing-in
-    4. Getting Help
-    5.Design & Performance
-    6. Depositing
-    7. Finding Games
-    8. Playing Games
-    9. Withdrawal
-    10. Using Bonus and Promotions. 
+    1. Entry and Home page related: Initial interaction with the website, including first impressions and overall layout navigation.
+    2. Registration: The process of creating a new user account, including entering personal information and completing verification steps.
+    3. Signing-in: The action of logging into an existing account, involving username and password input or other authentication methods.
+    4. Getting Help: Seeking assistance through customer support or navigating the help section for troubleshooting and inquiries.
+    5. Design & Performance: The visual appeal, usability, and technical efficiency of the website, including load times and responsiveness.
+    6. Depositing: Adding funds to a user account through various payment methods.
+    7. Finding Games: Browsing and selecting from available game options on the platform.
+    8. Playing Games: The experience of engaging with and participating in games on the site.
+    9. Withdrawal: The process of removing funds from a user account to a bank or other payment method.
+    10. Using Bonus and Promotions: Accessing and applying special offers, bonuses, and promotional deals provided by the platform.
+    If a review doesn't fit into any of the above categories, classify it under a new category called "General."
     """,
     verbose=True,
     llm = llm
@@ -45,7 +48,7 @@ reviewes_examiner = Agent(
 
 reviews = Task(
     description=dedent(f"""
-        Please conduct a comprehensive analysis of stake.com using Trustpilot.com, ensuring coverage of all languages by adding "?languages=all" to the URL parameter.Strictly Navigate through each paginated page of reviews by adding parameters like '?page=2', '?page=3', up to '?page=2' to ensure thorough research."""),
+        Please conduct a comprehensive analysis of {url} using Trustpilot.com, ensuring coverage of all languages by adding "?languages=all" to the URL parameter.Strictly Navigate through each paginated page of reviews by adding parameters like '?page=2', '?page=3', up to '?page=2' to ensure thorough research."""),
     expected_output=dedent(f"""A list of 20 reviews you've read from Trustpilot.com, along with user I would also like to get the reviews given by them and it should be a full review not the truncated ones.
       Please ensure your response includes specific examples or anecdotes from reviews to support your analysis and provide a nuanced understanding of user sentiment.                       
     """),
